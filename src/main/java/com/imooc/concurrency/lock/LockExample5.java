faa2e5ce8e0880cc300008c6ee391e61118e78fc
zty200329 <1412306160@qq.com>
2020-04-06 20:37:46 +0800

    并发学习

---
+++ src/main/java/com/imooc/concurrency/lock/LockExample5.java
@@ -0,0 +1,59 @@
+package com.imooc.concurrency.lock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * @Author zty
+ * @Date 2020/3/9 下午6:44
+ * @Description:StampedLock
+ */
+@Slf4j
+public class LockExample5 {
+    public static int cilentTotal = 5000;
+
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private final static StampedLock lock = new StampedLock();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+
+        final CountDownLatch countDownLatch = new CountDownLatch(cilentTotal);
+        for (int i = 0; i < cilentTotal; i++){
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",count);
+    }
+
+    private static void add(){
+        long stamp = lock.writeLock();
+        try {
+            count++;
+        }finally {
+            lock.unlock(stamp);
+        }
+    }
+}
